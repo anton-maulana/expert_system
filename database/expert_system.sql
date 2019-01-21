119ecf4006ae871e44ef72bdabb593a693c4134c
anton-maulana <anton_maulana@outlook.com>
2019-01-21 08:07:04 +0700

    update database

--- database/expert_system.sql
+++ database/expert_system.sql
@@ -1,61 +1,33 @@
 -- --------------------------------------------------------
--- Host:                         localhost
--- Server version:               10.2.14-MariaDB-10.2.14+maria~jessie - mariadb.org binary distribution
--- Server OS:                    debian-linux-gnu
--- HeidiSQL Version:             9.3.0.4984
+-- Host:                         127.0.0.1
+-- Server version:               5.7.24 - MySQL Community Server (GPL)
+-- Server OS:                    Linux
+-- HeidiSQL Version:             9.5.0.5338
 -- --------------------------------------------------------
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET NAMES utf8mb4 */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+
 
 -- Dumping database structure for expert_system
 CREATE DATABASE IF NOT EXISTS `expert_system` /*!40100 DEFAULT CHARACTER SET latin1 */;
 USE `expert_system`;
 
-
 -- Dumping structure for table expert_system.diagnosis
 CREATE TABLE IF NOT EXISTS `diagnosis` (
-  `id` int(11) NOT NULL,
-  `fk_diagnosis_group` int(11) DEFAULT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(255) DEFAULT NULL,
-  `descriptions` text DEFAULT NULL,
-  KEY `Index 1` (`id`),
-  KEY `FK__diagnosis_group` (`fk_diagnosis_group`),
-  CONSTRAINT `FK__diagnosis_group` FOREIGN KEY (`fk_diagnosis_group`) REFERENCES `diagnosis_group` (`id`)
+  `description` text,
+  `probability` float DEFAULT NULL,
+  `date_created` text,
+  `date_modified` text,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- Data exporting was unselected.
-
-
--- Dumping structure for table expert_system.diagnosis_group
-CREATE TABLE IF NOT EXISTS `diagnosis_group` (
-  `id` int(11) NOT NULL,
-  `level` int(11) DEFAULT NULL,
-  `diagnosis` varchar(255) DEFAULT NULL,
-  `parent_diagnosis` int(11) DEFAULT NULL,
-  `date_created` datetime DEFAULT NULL,
-  `date_modified` datetime DEFAULT NULL,
-  KEY `Index 1` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- Data exporting was unselected.
-
-
--- Dumping structure for table expert_system.display_symptoms
-CREATE TABLE IF NOT EXISTS `display_symptoms` (
-  `id` int(11) NOT NULL,
-  `level` int(11) DEFAULT NULL,
-  `symptoms_group` varchar(255) DEFAULT NULL,
-  `date_created` datetime DEFAULT NULL,
-  `date_modified` datetime DEFAULT NULL,
-  KEY `Index 1` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- Data exporting was unselected.
-
-
 -- Dumping structure for table expert_system.symptoms
 CREATE TABLE IF NOT EXISTS `symptoms` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -63,7 +35,18 @@ CREATE TABLE IF NOT EXISTS `symptoms` (
   `name` varchar(255) DEFAULT NULL,
   `date_created` datetime DEFAULT NULL,
   `date_modified` datetime DEFAULT NULL,
-  KEY `Index 1` (`id`)
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+
+-- Data exporting was unselected.
+-- Dumping structure for table expert_system.tree_symptoms_diagnosis
+CREATE TABLE IF NOT EXISTS `tree_symptoms_diagnosis` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `fk_symptoms_id` int(11) DEFAULT NULL,
+  `fk_diagnosis_id` int(11) DEFAULT NULL,
+  `date_created` datetime DEFAULT NULL,
+  `date_modified` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- Data exporting was unselected.
